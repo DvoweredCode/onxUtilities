bb57c0cffc688c993303cc25d5b36054071d6731
Jared Barranco <barrancojared@gmail.com>
2020-07-29 13:22:36 -0700

    Initial Commit
    Extension working - need to refactor names and limit usage to correct URL

---
+++ popup.js
@@ -0,0 +1,25 @@
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    if (request.action == "getSource") {
+        chrome.tabs.create({
+            url: request.source
+        })
+    }
+});
+
+function onWindowLoad() {
+
+    var message = document.querySelector('#message');
+
+    chrome.tabs.executeScript(null, {
+        file: "getPagesSource.js"
+    }, function (results) {
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError) {
+            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        }
+
+    });
+
+}
+
+window.onload = onWindowLoad;
