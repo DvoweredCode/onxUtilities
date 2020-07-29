d0deb10a3be619a8148ce3cc9a4365969df1ae40
Jared Barranco <barrancojared@gmail.com>
2020-07-29 13:45:22 -0700

    Refactoring file names
    Refactor file names

--- earthLink.js
+++ earthLink.js
@@ -11,7 +11,7 @@ function onWindowLoad() {
     var message = document.querySelector('#message');
 
     chrome.tabs.executeScript(null, {
-        file: "getPagesSource.js"
+        file: "getGoogleEarthUrl.js"
     }, function (results) {
         // If you try and inject into an extensions page or the webstore/NTP you'll get an error
         if (chrome.runtime.lastError) {
--- getGoogleEarthUrl.js
+++ getGoogleEarthUrl.js
@@ -1,7 +1,7 @@
 // @author Rob W <http://stackoverflow.com/users/938089/rob-w>
 // Demo: var serialized_html = DOMtoString(document);
 
-function DOMtoString(document_root) {
+function getEarthUrlForWaypoint(document_root) {
     var div = document_root.getElementsByClassName("markup-location-latlon")
     var hyperlink = " https://earth.google.com/web/search/" + div[0].innerText.substr(0, 9) + div[0].innerText.substr(10, 10) + "/"
     console.log(div)
@@ -10,5 +10,5 @@ function DOMtoString(document_root) {
 }
 chrome.runtime.sendMessage({
     action: "getSource",
-    source: DOMtoString(document)
+    source: getEarthUrlForWaypoint(document)
 });
